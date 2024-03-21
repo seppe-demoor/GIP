e11f358dd4124c106a7042bddb54981b35b9ec32
Seppe De Moor <seppe.demoor@leerling.go-ao.be>
2024-03-21 13:45:22 +0100

    update

--- header.php
+++ header.php
@@ -12,6 +12,17 @@
         integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm"
         crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Scheduling</title>
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+        integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" href="./fullcalendar/lib/main.min.css">
+    <script src="./js/jquery-3.6.0.min.js"></script>
+    <script src="./js/bootstrap.min.js"></script>
+    <script src="./fullcalendar/lib/main.min.js"></script>
     <title>Dashboard</title>
     <style>
         body {
@@ -22,26 +33,70 @@
         }
 
         .navbar {
-            overflow: hidden;
+            height: 10%;
             background-color: #333;
         }
 
-        .navbar a {
-            float: left;
-            display: block;
-            color: white;
-            padding: 14px 16px;
+        .header-btn{
             text-decoration: none;
+            padding: 0.5rem 0.8rem;
+            background-color: red;
+            color: white;
+            border-radius: 0.1rem;
+            border: none;
+            margin: 0rem 0.5rem 0rem 0.5rem;
         }
 
         .navbar a:hover {
-            background-color: #555;
+            background-color: #f72314;
+            padding: 0.8rem;
+            color: white;
         }
 
         .navbar a.active {
-            background-color: #cd0000;
-            color: white;
+            background-color: white;
+            color: black;
         }
+
+
+
+/* The container <div> - needed to position the dropdown content */
+.dropdown {
+  position: relative;
+  display: inline-block;
+}
+.dropdown-btn{
+
+}
+/* Dropdown Content (Hidden by Default) */
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f1f1f1;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+  transition: all in 0.5s;
+}
+
+/* Links inside the dropdown */
+.dropdown-content a {
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+}
+
+/* Change color of dropdown links on hover */
+.dropdown-content a:hover {
+    background-color: #ddd;
+}
+
+/* Show the dropdown menu on hover */
+.dropdown:hover .dropdown-content {display: block;}
+
+/* Change the background color of the dropdown button when the dropdown content is shown */
+.dropdown:hover .dropbtn {background-color: #3e8e41;}
 
     </style>
         <link rel="icon" href= "NWNSoftware.png"  type="image/x-icon">
@@ -49,12 +104,17 @@
 <body>
 
 <div class="navbar">
-    <a class="active" href="homePage.php">HomePage</a>
+    <a class="active header-btn" href="homePage.php">Home</a>
 
     <?php if (isset($_SESSION['admin']) && $_SESSION['admin'] == true): ?>
-            <a href="userOverzicht.php">overzicht gebruikers</a>
-            <a href="countryOverzicht.php">overzicht landen</a>
-            <a href="customerOverzicht.php">overzicht Klanten</a>
+        <div class="dropdown">
+  <button class="header-btn">Overzicht</button>
+  <div class="dropdown-content">
+            <a class="dropdown-btn" href="userOverzicht.php">Gebruikers</a>
+            <a class="dropdown-btn" href="countryOverzicht.php">Landen</a>
+  </div>
+</div>
+           
         <!--<div class="dropdown">
             <button class="dropbtn">Users</button>
             <div class="dropdown-content">
@@ -70,11 +130,19 @@
     <?php ?>
 
     <?php if (!isset($_SESSION["admin"])) : ?>
-        <a href="loginPage.php">Login</a>
+        <a class="header-btn" href="loginPage.php">Login</a>
     <?php else : ?>
-        <a href="logoutPage.php" class="logout">Logout</a>
+        <a href="logoutPage.php" class="header-btn">Logout</a>
     <?php endif; ?>
 </div>
 
-</body>
-</html>
+
+
+ 
+
+
+Op 2024-03-21 13:36:15 schreef Seppe De Moor - 6INFO
+
+
+
+<bootstrap.min.css>
